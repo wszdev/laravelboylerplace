4badfa43ef37219812f3e22777567a85647940f0
Biplob Shaha <devbipu@gmail.com>
2022-03-20 16:26:31 +0600

    Best Laravel ui setup with auth

---
+++ app/Mail/TestMail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $getDtats;
+
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+
+    
+    public function __construct($data)
+    {
+        $this->getDtats = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject("this is a test mail")->view('mailsend');
+    }
+}
